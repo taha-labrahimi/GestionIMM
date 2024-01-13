4d9ffda1fb597968215b14961b5f45f225847f23
taha labrahimi <taha.labrahimi.devl@gmail.com>
2024-01-13 19:10:07 +0100

    add classes

---
+++ Models/Client.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionIMM.Models
+{
+
+   
+        // Définition de l'énumération pour les types de clients
+        public enum TypeClient
+        {
+            Acheteur,
+            Vendeur,
+            Locataire
+        }
+
+        public class Client
+        {
+            [Key]
+            public int Id { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            public string Nom { get; set; }
+
+            [Required]
+            [StringLength(100)]
+            public string Prenom { get; set; }
+
+            [Required]
+            [Phone]
+            public string NumeroTelephone { get; set; }
+
+            [Required]
+            [StringLength(255)]
+            public string Adresse { get; set; }
+
+            public TypeClient Type { get; set; }
+        }
+}
---
+++ Models/Propriete.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionIMM.Models
+{
+    public class Propriete
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        public double Taille { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Emplacement { get; set; }
+
+        public string Caracteristiques { get; set; }
+
+        public byte[] Image { get; set; }
+    }
+}
